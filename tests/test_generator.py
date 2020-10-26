a3b268a6880f4c15f76a9c54c662fa198b313026
thengo1 <tbn2109@columbia.edu>
2020-10-25 20:26:43 -0400

    Adding five test cases

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -4,17 +4,17 @@ import filecmp
 
 def test_markdown_exists():
     generator()
-    assert path.exists("../generator/book/test.md") == True
+    assert path.exists("./generator/book/test.md") == True
 
 def test_generator_ouput_exists():
     generator()
-    assert path.exists("../generator/public/test.html") == True
+    assert path.exists("./generator/public/test.html") == True
 
 def test_html_file():
-    assert filecmp.cmp('./test.html', '../generator/public/test.html') == True
+    assert filecmp.cmp('./tests/test.html', './generator/public/test.html') == True
 
 def test_public_directory_exists():
-    assert path.exists("../generator/public") == True
+    assert path.exists("./generator/public") == True
 
 def test_book_directory_exists():
-    assert path.exists("../generator/book") == True
+    assert path.exists("./generator/book") == True
